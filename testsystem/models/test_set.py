eed8d064c58eedd6ef6c71b72de82444af92a623
Klaus Weinbauer <klaus.weinbauer@student.tugraz.at>
2023-10-13 15:07:48 +0200

    Fix SQL type

--- testsystem/models/test_set.py
+++ testsystem/models/test_set.py
@@ -1,21 +1,21 @@
 #
 # Copyright 2023 EAS Group
 #
-# Permission is hereby granted, free of charge, to any person obtaining a copy of this 
-# software and associated documentation files (the “Software”), to deal in the Software 
-# without restriction, including without limitation the rights to use, copy, modify, 
-# merge, publish, distribute, sublicense, and/or sell copies of the Software, and to 
-# permit persons to whom the Software is furnished to do so, subject to the following 
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this
+# software and associated documentation files (the “Software”), to deal in the Software
+# without restriction, including without limitation the rights to use, copy, modify,
+# merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to the following
 # conditions:
 #
-# The above copyright notice and this permission notice shall be included in all copies 
+# The above copyright notice and this permission notice shall be included in all copies
 # or substantial portions of the Software.
 #
-# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
-# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
-# PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
-# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
-# CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE 
+# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+# PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+# CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
 # OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #
 
@@ -26,7 +26,7 @@ import time
 import string
 import testsystem.db as db
 
-from sqlalchemy import Column, Integer, String, ForeignKey, Boolean
+from sqlalchemy import Column, Integer, String, ForeignKey, Boolean, BigInteger
 from sqlalchemy.orm import Session, relationship, joinedload
 
 from .test_result import TestResult
@@ -134,9 +134,9 @@ class TestSet(db.Base):
     group_id: int = Column(Integer, ForeignKey("Groups.id"), nullable=False)  # type: ignore
     commit_hash: str = Column(String(40), nullable=False, unique=True)  # type: ignore
     commit_message: str = Column(String(50), nullable=True)  # type: ignore
-    commit_time: int = Column(Integer, nullable=True)  # type: ignore
+    commit_time: int = Column(BigInteger, nullable=True)  # type: ignore
     finished: bool = Column(Boolean, nullable=False, default=False)  # type: ignore
-    timestamp: int = Column(Integer, nullable=False)  # type: ignore
+    timestamp: int = Column(BigInteger, nullable=False)  # type: ignore
 
     test_results: list[TestResult] = relationship(
         "TestResult",
